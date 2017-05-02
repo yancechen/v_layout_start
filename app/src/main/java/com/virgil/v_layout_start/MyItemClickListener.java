7acdea717d79d57aa859c10bc556a84855c96c45
有余 <841864388@qq.com>
2017-05-02 16:10:38 +0800

    chenyouyu 2017-05-02-01 测试

---
+++ app/src/main/java/com/virgil/v_layout_start/MyItemClickListener.java
@@ -0,0 +1,14 @@
+package com.virgil.v_layout_start;
+
+import android.view.View;
+
+/**
+ * 2016 逛街购（北京）网络科技有限公司，版权所有
+ * guangjiegou - Android客户端
+ * Context：
+ * Created by chenyouyu on 2017/5/2 10:57.
+ */
+
+public interface MyItemClickListener {
+    public void onItemClick(View view, int postion);
+}
